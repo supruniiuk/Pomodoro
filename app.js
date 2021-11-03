7d155f47b43b32af11a6276f0b29cf42f751a4cb
supruniiuk <juliasupruniuk23@gmail.com>
2021-11-03 15:27:48 +0200

    added simple layout and timer

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("hello pomodoro!")
+let time = 2;
+let repeat_time = 1000;
+
+const convert_to_seconds = (min, sec) => {
+  return Number(min * 60 + Number(sec));
+};
+
+const convert_from_seconds = (seconds) => {
+  const sec = seconds % 60;
+  const min = Math.floor(seconds / 60);
+  return [min, sec];
+};
+
+const showTime = () => {
+  let minutes = document.querySelector("#min");
+  let seconds = document.querySelector("#sec");
+  if (time > 0) {
+    --time;
+    let [min, sec] = convert_from_seconds(time);
+
+    minutes.textContent = String(min).padStart(2, "0");
+    seconds.textContent = String(sec).padStart(2, "0");
+  }
+};
+setInterval(showTime, repeat_time);
