7358bbacbccdfe99fb5fdf632e5e23c2b74a30dd
yuliiasupruniuk <juliasupruniuk23@gmail.com>
2022-01-25 22:09:07 +0200

    made responsive view

--- js/app.js
+++ js/app.js
@@ -8,17 +8,6 @@ const convert_from_seconds = (seconds) => {
   return [min, sec];
 };
 
-let time = 1500;
-let currentTime = 1500;
-const repeat_time = 1000;
-let counterActive = false;
-const timeTypes = {
-  pomodoro: 1500,
-  shortBreak: 300,
-  longBreak: 600,
-};
-let intrvl;
-
 const minutes = document.querySelector("#min");
 const seconds = document.querySelector("#sec");
 const timePicker = document.querySelectorAll("[data-time-type]");
@@ -26,6 +15,17 @@ const timePicker = document.querySelectorAll("[data-time-type]");
 const startBtn = document.querySelector('[data-action="start"]');
 const pauseBtn = document.querySelector('[data-action="pause"]');
 const resetBtn = document.querySelector('[data-action="reset"]');
+
+let time = 1500;
+let currentTime = 1500;
+const repeat_time = 1000;
+const timeTypes = {
+  pomodoro: 1500,
+  shortBreak: 300,
+  longBreak: 600,
+};
+let intrvl;
+
 
 const showTime = () => {
   if (currentTime > 0) {
@@ -41,6 +41,7 @@ const setTime = (duration) => {
   currentTime = duration;
 };
 
+
 const startTimer = () => {
   intrvl = setInterval(showTime, repeat_time);
 };
@@ -55,16 +56,18 @@ const resetTimer = () => {
   showTime();
 };
 
+
 for (let i = 0; i < timePicker.length; i++) {
   const element = timePicker[i];
   const duration = element.getAttribute("data-time-type");
-  console.log(duration, timeTypes[duration]);
 
   element.onclick = () => {
+    resetTimer();
     setTime(timeTypes[duration]);
     showTime(timeTypes[duration]);
   };
 }
+
 
 startBtn.onclick = () => {
   startTimer();
